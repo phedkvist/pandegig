54f822c55998eeade0990df68d6d0c816c0c01b6
Pierre Hedkvist <pierre.hedkvist@gmail.com>
2020-04-03 20:08:23 +0200

    Add ESLint

---
+++ pandegig/.eslintrc.js
@@ -0,0 +1,38 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    'react-hooks'
+  ],
+  rules: {
+    "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
+    "react/prop-types": ["error", { "ignore": ["navigation"] }],
+    "react-hooks/exhaustive-deps": "warn",
+    "react/jsx-no-bind": ["warn", {
+      "ignoreDOMComponents": false,
+      "ignoreRefs": false,
+      "allowArrowFunctions": false,
+      "allowFunctions": false,
+      "allowBind": false
+    }]
+
+  },
+};
