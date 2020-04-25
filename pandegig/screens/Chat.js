3f6eff79898ecca09504cfe3e5a492b52e4c4045
Pierre Hedkvist <pierre.hedkvist@gmail.com>
2020-04-25 13:09:38 +0200

    Add chat text input that sticks to the bottom similar to the messenger app

--- pandegig/screens/Chat.js
+++ pandegig/screens/Chat.js
@@ -1,9 +1,10 @@
-import React from 'react';
-import { KeyboardAvoidingView, Platform, View, Text, StyleSheet, TextInput, TouchableWithoutFeedback, Keyboard } from 'react-native';
+import React, { useState, useCallback } from 'react';
+import { Platform, View, Text, StyleSheet, TextInput, TouchableWithoutFeedback, Keyboard, Button } from 'react-native';
 import Message from '../components/Message';
 import { ScrollView } from 'react-native-gesture-handler';
-import Button from '../components/Button';
+// import Button from '../components/Button';
 import Input from '../components/Input';
+import { KeyboardAccessoryView } from 'react-native-keyboard-accessory';
 
 
 const MESSAGES = [
@@ -99,7 +100,7 @@ const MESSAGES = [
   },
 ];
 
-const style = StyleSheet.create({
+const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'space-between',
@@ -118,37 +119,70 @@ const style = StyleSheet.create({
   },
   submitButton: {
     marginTop: 6,
+  },
+  textInputView: {
+    padding: 8,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+  },
+  textInput: {
+    flexGrow: 1,
+    borderWidth: 1,
+    borderRadius: 10,
+    borderColor: '#CCC',
+    padding: 10,
+    fontSize: 16,
+    marginRight: 10,
+    textAlignVertical: 'top'
+  },
+  textInputButton: {
+    flexShrink: 1,
   }
 });
 
 const Chat = ({ navigation, messages = MESSAGES }) => {
   const name = navigation.getParam('name', '');
+  const [inputHeight, setInputHeight] = useState(35);
+  const [input, setInput] = useState('');
+  const onChangeText = useCallback((text) => setInput(text), [setInput]);
+  const onContentSizeChange = useCallback((event) => setInputHeight(event.nativeEvent.contentSize.height), [setInputHeight]);
   return (
-    <KeyboardAvoidingView
-      behavior={Platform.OS == "ios" ? "padding" : "height"}
-      style={style.container}
+    <View
+      style={styles.container}
     >
-      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <TouchableWithoutFeedback
+        onPress={Keyboard.dismiss}
+      >
         <>
           <Text>{name}</Text>
-          <ScrollView style={style.messageContainer}>
+          <ScrollView style={styles.messageContainer}>
             {
               messages.map(c =>
                 <Message content={c.content} createdAt={c.createAt} status={c.status} isRecieved={c.fromUserId === 'a'} />
               )
             }
           </ScrollView>
-          <View style={style.inputContainer}>
-            <View style={style.input}>
-              <Input />
+          <KeyboardAccessoryView alwaysVisible={true}>
+            <View style={styles.textInputView}>
+              <TextInput
+                value={input}
+                underlineColorAndroid="transparent"
+                style={[styles.textInput, { height: Math.max(35, inputHeight) + 5 }]}
+                multiline={true}
+                onChangeText={onChangeText}
+                onContentSizeChange={onContentSizeChange}
+              />
+              <Button
+                style={styles.textInputButton}
+                title="Send"
+                onPress={() => { }} />
             </View>
-            <View style={style.submitButton}>
-              <Button>Submit</Button>
-            </View>
-          </View>
+          </KeyboardAccessoryView>
         </>
       </TouchableWithoutFeedback>
-    </KeyboardAvoidingView>
+    </View>
   );
 };
 
